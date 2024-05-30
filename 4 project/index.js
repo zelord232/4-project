2467adc0a3021cde1f2d3ed3da9b296a6b8f200c
zelord232 <150454002+zelord232@users.noreply.github.com>
2024-05-30 13:09:08 +0300

    123
    123

--- 4 project/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
---
+++ Frontend-pindie-main/README.md
@@ -0,0 +1,1 @@
+Мой бэкенд:https://github.com/Zabava2/Pindie-backend.git
